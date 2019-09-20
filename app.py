e327de4900dd005150337f990fd90ae9df60da65
Juan Sebastián Cadena Rodríguez <juansc@lagash.com>
2019-09-20 14:40:47 -0500

    Initial login module

--- app.py
+++ app.py
@@ -70,6 +70,35 @@ def register():
 
     return render_template('register.html', form=form)
 
+# User Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+       # Get Form Values
+       username = request.form['username']
+       password_candidate = request.form['password']
+
+       # Create cursor
+       cur = mysql.connection.cursor()
+
+       # Get user by username
+       result = cur.execute('SELECT * FROM users WHERE username = %s', [username])
+
+       if result > 0:
+           # Get stored hash
+           data = cur.fetchone()
+           password = data['password']
+
+           # Compare passwords
+           if sha256_crypt.verify(password_candidate, password):
+               app.logger.info('PASSWORD MATCHED')
+           else:
+               app.logger.info('PASSWORD NOT MATCHED')
+
+       else:
+            app.logger.info('NO USER')
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
